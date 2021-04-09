6f083d4640c8e0b972844967c3f0d9224d2de009
andressafmachado <andressamachado98@gmail.com>
2021-04-09 19:03:52 +0200

    set up signup for Doctors

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -67,6 +67,32 @@ export const signUp = (
   };
 };
 
+export const signUpDoctor = (name, email, password) => {
+  return async (dispatch, getState) => {
+    dispatch(appLoading());
+    try {
+      const response = await axios.post(`${apiUrl}/signupdoctor`, {
+        name,
+        email,
+        password,
+      });
+
+      dispatch(loginSuccess(response.data));
+      dispatch(showMessageWithTimeout("success", true, "account created"));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        dispatch(setMessage("danger", true, error.response.data.message));
+      } else {
+        console.log(error.message);
+        dispatch(setMessage("danger", true, error.message));
+      }
+      dispatch(appDoneLoading());
+    }
+  };
+};
+
 export const login = (email, password) => {
   return async (dispatch, getState) => {
     dispatch(appLoading());
