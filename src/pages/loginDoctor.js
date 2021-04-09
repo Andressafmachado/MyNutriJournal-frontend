185a567d376cda509705b38446f3c404e0bb0922
andressafmachado <andressamachado98@gmail.com>
2021-04-09 18:43:59 +0200

    Logindoctor page frontend

---
+++ src/pages/loginDoctor.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { login } from "../store/user/actions";
+import { selectToken } from "../store/user/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, Link } from "react-router-dom";
+import { Col } from "react-bootstrap";
+import { selectUser } from "../store/user/selectors";
+
+export default function LoginDoctor() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  function submitForm(event) {
+    console.log("hi");
+    event.preventDefault();
+
+    dispatch(login(email, password));
+
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <Container>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+        <h1 className="mt-5 mb-5">Welcome Doctor</h1>
+        <h5>Login</h5>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mt-5">
+          <Button variant="primary" type="submit" onClick={submitForm}>
+            Log in
+          </Button>
+        </Form.Group>
+        <h6>
+          Don't have an account yet?
+          <Link to="/signupdoctor"> sign up here</Link>{" "}
+        </h6>
+      </Form>
+    </Container>
+  );
+}
